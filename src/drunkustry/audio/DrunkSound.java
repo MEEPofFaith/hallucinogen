de7dc1141692d93144902984485d3388757b3db3
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2024-02-06 21:32:18 -0800

    Drunker audio

--- src/drunkustry/audio/DrunkSound.java
+++ src/drunkustry/audio/DrunkSound.java
@@ -1,14 +1,23 @@
 package drunkustry.audio;
 
 import arc.*;
+import arc.audio.*;
+import arc.audio.Filters.*;
 import arc.math.*;
 import mindustry.game.EventType.*;
 
 public class DrunkSound{
+    public static final FlangerFilter filter = new FlangerFilter(){{
+        set(0.2f, 0.4f);
+    }};
+
     public static void init(){
         Events.run(Trigger.update, () -> {
             Core.audio.globalPitch = 1.001f + Mathf.sin(150 * drunkScl() / Mathf.PI2, 0.5f * drunkMag());
         });
+
+        Core.audio.setFilter(0, filter);
+        Core.audio.setFilterParam(0, 0, Filters.paramWet, 1f);
     }
 
     public static float drunkMag(){
